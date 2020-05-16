66f6d8dd155b220d88ef5063dfc4b2d0592bd32e
Yutaro Mori <yutaro@umaproject.org>
2020-05-16 22:22:06 +0900

    Scaffolding for base components

---
+++ src/Coins.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Coins() {
+    return (
+        <div className="Coins">
+            Coins
+        </div>
+    )
+}
+
+export default Coins;
---
+++ src/Quiz.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Quiz() {
+    return (
+        <div className="Quiz">
+            Quiz
+        </div>
+    )
+}
+
+export default Quiz;
