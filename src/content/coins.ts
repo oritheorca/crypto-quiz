afb50f72a1f3e3a60d16ec0ca4603f7d439178f5
Yutaro Mori <yutaro@umaproject.org>
2020-05-23 12:31:09 +0900

    Push descriptions for each coin.:

--- src/content/coins.ts
+++ src/content/coins.ts
@@ -9,4 +9,30 @@ const coins = [
   "Zcash",
 ];
 
+const descriptions = {
+  Bitcoin:
+    "You're the dependable class teacher that's been at the school forever. Bitcoin, of course, is the original cryptocurrency that started it all. Although it lacks the features that the newer coins have, Bitcoin puts an emphasis on small improvements in order to avoid breaking changes. Nowadays, Bitcoin is marketed as digital gold: a reliable store of money.",
+
+  "Bitcoin Cash":
+    "You're the substitute teacher that's in the shadow of the class teacher (Bitcoin). Bitcoin Cash forked off from Bitcoin in August, 2017. Although on the surface the dispute was about whether or not to increase transaction throughput, there were also deeper differences of culture and the direction of the technical roadmap. Nowadays, Bitcoin Cash markets itself as the payments friendly alternative to Bitcoin.",
+
+  Ethereum:
+    "You're the hacker with a fun streak. Ethereum started as an alternative to Bitcoin that also supports a programmable logic. Whereas Bitcoin is mostly limited to transactions from one person to another, Ethereum can support complex ideas such as decentralized marketplaces and distributed organizations. Nowadays, Ethereum is working on a complete overhaul to bring more speed, reliability, and sustainability to Ethereum.",
+
+  Libra:
+    "You're the school jock. Libra is a cryptocurrency with low transaction fees that's designed to help with everyday transactions, especially across borders. Unlike most other cyrptocurrencies, Libra doesn't have a native token, but coins who's value is tied to currencies that people already use every day. Libra began as a Facebook project, and therefore has the most legitimacy amongst all cryptocurrencies. Its scheduled to be released some time this year.",
+
+  "Binance Coin":
+    "You're the tech and social media savvy student. Binance itself is a cryptocurrency exchange that issued it's own token that users can invest or speculate in. Binance itself quickly became the highest volume exchange in the world, and the price of Binance Coin quickly rose with it. Nowadays, Binance is the most profitable company in the entire cryptocurrency industry.",
+
+  Dogecoin:
+    "You're the goofy class clown. Dogecoin is a cryptocurrency based solely on an internet meme. Despite the unseriousness of the coin, it somehow still maintains a market cap of $300 million.",
+
+  Zcash:
+    "You're the kid in the computer lab working on some science project that no one else understands. Zcash is a cryptocurrency that uses an advanced cryptographic primitive called Zero Knowledge Proofs. Unlike other cryptocurrencies, in which the details of each transaction is public, Zcash allows transactions to be made privately. Nowadays, Zcash is continuing to improve these cryptographic techniques for everyday usecases.",
+
+  Ripple:
+    "You're the hustler that's building a following on social media for profit. Rippe is a cryptocurrency that's marketed for low cast transactions, particularly between banks.",
+};
+
 export default coins;
